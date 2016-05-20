293fc94ce00b42b227737474a65998fbed0d0195
Kim Concha <kim_concha@launchlabs.ph>
2016-05-20 18:28:10 +0800

    Developed basic input output sample Exercises
    Signed-off-by: Kim Concha <kim_concha@launchlabs.ph>

--- BasicInputOutputSample/BasicInputOutputSample/TestExamples/BasicOperations.cs
+++ BasicInputOutputSample/BasicInputOutputSample/TestExamples/BasicOperations.cs
@@ -6,42 +6,48 @@ namespace BasicInputOutputSample
 {
 	public class BasicOperations : ContentPage
 	{
-		Label output; 
-
 		public BasicOperations ()
 		{
+			Label output = new Label();
+			double firstNum = 0.0;
+			double secondNum = 0.0;
+
 			var firstEntry = new Entry () { Placeholder = "First Number" };
+			firstEntry.TextChanged += (sender, e) => 
+			{
+			 firstNum = Convert.ToDouble(e.NewTextValue);
+			};
 
 			var secondEntry = new Entry () { Placeholder = "Second Number" };
+			secondEntry.TextChanged += (sender, e) => 
+			{
+				secondNum = Convert.ToDouble(e.NewTextValue);
+			};
 
 
 			// Buttons
-			var sumButton = new Button () {
-				Text = "SUM"
-			};
+			var sumButton = new Button () { Text = "SUM" };
 			sumButton.Clicked += (sender, e) => {
-				
+				double sum = firstNum + secondNum;
+				output.Text = "Sum is " + sum.ToString();
 			};
 
-			var diffButton = new Button () {
-				Text = "DIFFERENCE"
-			};
+			var diffButton = new Button () { Text = "DIFFERENCE" };
 			diffButton.Clicked += (sender, e) => {
-				
+				double diff = firstNum - secondNum;
+				output.Text = "Difference is " + diff.ToString();
 			};
 
-			var proButton = new Button () {
-				Text = "PRODUCT"
-			};
+			var proButton = new Button () { Text = "PRODUCT" };
 			proButton.Clicked += (sender, e) => {
-
+				double pro = firstNum * secondNum;
+				output.Text = "Product is " + pro.ToString();
 			};
 
-			var qouButton = new Button () {
-				Text = "QOUTIENT"
-			};
+			var qouButton = new Button () { Text = "QOUTIENT" };
 			qouButton.Clicked += (sender, e) => {
-
+				double qou = firstNum / secondNum;
+				output.Text = "Qoutient is " + qou.ToString();
 			};
 			// End Buttons
 
