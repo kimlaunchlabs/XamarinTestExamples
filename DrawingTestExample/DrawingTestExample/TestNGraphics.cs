e7ed1127487cfbc53138be71b40c72c2bc85ce64
Kim Concha <kim_concha@launchlabs.ph>
2016-05-31 18:56:48 +0800

    Minor Code Updates
    Signed-off-by: Kim Concha <kim_concha@launchlabs.ph>

---
+++ DrawingTestExample/DrawingTestExample/TestNGraphics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+using NGraphics;
+using NControl.Abstractions;
+
+namespace DrawingTestExample
+{
+	public class TestNGraphics : ContentPage
+	{
+		public TestNGraphics ()
+		{
+//			Content = new NControlView {
+//				DrawingFunction = (canvas, rect) => {
+//					canvas.DrawLine(rect.Left, rect.Top, rect.Width, rect.Height, NGraphics.Colors.Red);
+//					canvas.DrawLine(rect.Width, rect.Top, rect.Left, rect.Height, NGraphics.Colors.Yellow);
+//				}
+//			};
+
+
+
+		}
+	}
+}
+
+
---
+++ DrawingTestExample/iOS/AppDelegate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+//using NControl.iOS;
+
+namespace DrawingTestExample.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+			LoadApplication (new App ());
+
+			return base.FinishedLaunching (app, options);
+
+
+
+
+//			global::Xamarin.Forms.Forms.Init ();
+//			Xamarin.FormsMaps.Init();
+//			NControlViewRenderer.Init();
+//
+//			LoadApplication (new App (new ContainerProvider(), (container) => {
+//
+//				// Register providers
+//				container.Register<IImageProvider, ImageProvider>();
+//
+//			}));
+//
+//			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
